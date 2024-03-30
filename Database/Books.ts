bf0b9831b04461deb1f6cad0c83b5e2f0d1e1716
Nour Hallak <nour.n.hallak@gmail.com>
2024-03-30 23:57:34 +0200

    added Database

---
+++ Database/Books.ts
@@ -0,0 +1,49 @@
+interface book{
+    name:string;
+    auther:string;
+    decoded_string:string;
+    location_x: number;
+    location_y: number;
+    location_z: number;
+}
+
+
+let Books: book[] = [
+    {
+        name: "The best book in the world",
+        auther: "Best Auther",
+        decoded_string: "123abc",
+        location_x: 5.4, 
+        location_y: 5.4, 
+        location_z: 5.4, 
+
+    },
+    {
+        name: "The 2nd best book in the world",
+        auther: "2nd Best Auther",
+        decoded_string: "n98d3c",
+        location_x: 5.4, 
+        location_y: 5.4, 
+        location_z: 5.4, 
+
+    },
+    {
+        name: "The 3rd best book in the world",
+        auther: "3rd Best Auther",
+        decoded_string: "k3908v",
+        location_x: 5.4, 
+        location_y: 5.4, 
+        location_z: 5.4, 
+
+    },
+    {
+        name: "The 4th best book in the world",
+        auther: "4th Best Auther",
+        decoded_string: "coi78h",
+        location_x: 5.4, 
+        location_y: 5.4, 
+        location_z: 5.4, 
+
+    },
+    
+]
