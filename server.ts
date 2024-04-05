965b1cd4270dbee6e4ec76f36ac76c915ae2fc03
Nour Hallak <nour.n.hallak@gmail.com>
2024-04-05 17:17:21 +0300

    idk

--- server.ts
+++ server.ts
@@ -1,13 +1,19 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 const dotenv = require("dotenv").config();
 
 const app = express();
 // Parse data
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/screen", require("./routes/ScreenRouter"));
 
-const port = process.env.port || 5000;
+const port = process.env.port;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
