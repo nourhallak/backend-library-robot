99a30729678c947f733a8a0c76f709e806fed880
Nour Hallak <nour.n.hallak@gmail.com>
2024-04-14 19:02:27 +0300

    commented odom pose twist

--- services/rosService/rosSubscribers.ts
+++ services/rosService/rosSubscribers.ts
@@ -11,39 +11,6 @@ import {
 export const rosSubscribers = (ros: ROSLIB.Ros) => {
   const robotState = RobotState.getInstance();
 
-  // // odom
-  // const odomListener = new ROSLIB.Topic({
-  //   ros: ros,
-  //   name: "/odom",
-  //   messageType: "nav_msgs/Odometry",
-  // });
-
-  // odomListener.subscribe(function (message: any) {
-  //   robotState.position = (message as OdomMessage).pose.pose.position;
-  // });
-
-  // // pose
-  // const poseListener = new ROSLIB.Topic({
-  //   ros: ros,
-  //   name: "/pose",
-  //   messageType: "geometry_msgs/Pose",
-  // });
-
-  // poseListener.subscribe(function (message: any) {
-  //   robotState.position = (message as PoseMessage).position;
-  // });
-
-  // // twist
-  // const twistListener = new ROSLIB.Topic({
-  //   ros: ros,
-  //   name: "/twist",
-  //   messageType: "geometry_msgs/twist",
-  // });
-
-  // twistListener.subscribe(function (message: any) {
-  //   // robotState.position  = (message as Twis;
-  // });
-
   // battery percentage
   const batteryListener = new ROSLIB.Topic({
     ros: ros,
